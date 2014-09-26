d2824e200483a6a3b0c47438fc9a5987a732425c
Joseph Dorris <jdorris1331@gmail.com>
2014-09-26 13:22:03 -0400

    added mgl_wrapper code

--- mgl_interface/mgl_wrapper.cpp
+++ mgl_interface/mgl_wrapper.cpp
@@ -11,14 +11,31 @@ mgl_wrapper:~mgl_wrapper() {
   
 }
 
-mgl_wrapper:load_data(int size, double* x, double* y, double* z, double* c) {
-    
+void mgl_wrapper:load_data(int size, double* temp_x, double* temp_y, double* temp_z, double* temp_c) {
+    x = new mglData(size);
+    y = new mglData(size);
+    z = new mglData(size);
+    c = new mglData(size);
     for(int i=0;i<size;i++) {
-        mreal t=
+        x.a[i] = temp_x[i];
+        y.a[i] = temp_y[i];
+        z.a[i] = temp_z[i];
+        c.a[i] = temp_c[i];
     }        
 }
 
-mgl_wrapper:plot() {
-    gr = new mglQT();
+void mgl_wrapper:plot() {
+    gr = new mglQT(setup,"3D_PDE");
 }
 
+int mgl_wrapper:setup(mglGraph *gr) {
+    gr->Title("Test 1");  gr->Rotate(50,60);
+    gr->Box();  gr->Dots(x,y,z);
+    return 0;
+}
+/*
+int main() {
+    mgl_wrapper A;
+    A.setup(
+}
+*/
--- mgl_interface/mgl_wrapper.h
+++ mgl_interface/mgl_wrapper.h
@@ -2,19 +2,22 @@
 #define __MGL_WRAPPER__
 
 #include <iostream>
+#include <mgl2/qt.h>
 
 using namespace std;
 
+//class for interacting with the mathGL window
 class mgl_wrapper {
     public:
         mgl_wrapper();
         ~mgl_wrapper();
 
-        load_data(int size, double* x, double* y, double* z, double* c);
-	plot();
+        void load_data(int size, double* x, double* y, double* z, double* c);
+	void plot();
     private:
         mglQT* gr;
         mglData* x, y, z, c;
+        int setup(mglGraph *gr);
 };
 
 		
