c31d222788ef726e816521e902233edcf28449d3
Joseph Dorris <jdorris1331@gmail.com>
2014-11-05 12:34:36 -0500

    updated normalizer

--- normalizer/normalizer.cpp
+++ normalizer/normalizer.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main() {
   int num_vars = 3;
+  int dim_size = 100;
   int priority[3];
   priority[0]=2;
   priority[1]=0;
@@ -25,43 +26,40 @@ int main() {
   colors[1]=2.0;
   colors[2]=3.0;
   
-  int n=100*100*100;
+  //int n=100*100*100;
   int current;
-  mglData x(n),y(n),z(n),c(n);
+  mglData a(dim_size,dim_size,dim_size);
 
   double ****var;
-  var = new double***[3];
+  var = new double***[num_vars];
 
-  for(int i=0;i<3;i++) {
-    var[i] = new double**[100];
-      for(int j=0;j<100;j++) {
-	var[i][j] = new double*[100];
-	  for(int k=0;k<100;k++) {
-		var[i][j][k] = new double[100];
-		for(int l=0;l<100;l++) var[i][j][k][l] = 0;
+  for(int i=0;i<num_vars;i++) {
+    var[i] = new double**[dim_size];
+      for(int j=0;j<dim_size;j++) {
+	var[i][j] = new double*[dim_size];
+	  for(int k=0;k<dim_size;k++) {
+		var[i][j][k] = new double[dim_size];
+		for(int l=0;l<dim_size;l++) var[i][j][k][l] = 0;
 	  }
       }
   }
 
   srand(time(NULL));
 
-  for(int i=0;i<100;i++) {
-    for(int j=0;j<100;j++) {
-      for(int k=0;k<100;k++) {
-        current = (i*10000)+(j*100)+k;
+  for(int i=0;i<dim_size;i++) {
+    for(int j=0;j<dim_size;j++) {
+      for(int k=0;k<dim_size;k++) {
+        current = i+dim_size*(j+dim_size*k);
 //        cout << i << " " << j << " " << k << endl;
-        x.a[current] = i;
-        y.a[current] = j;
-        z.a[current] = k;
         var[0][i][j][k] = (double)(rand() % 500) / 10;
         var[1][i][j][k] = (double)(rand() % 2000) / 10 - 50.0;
         var[2][i][j][k] = (double)(rand() % 500) / 100 - 2.0;
         
         for(int l=0;l<num_vars;l++) {
           for(int m=0;m<num_vars;m++) {
-            if(priority[l] == m && c[current]==0) {
+            if(priority[l] == m && a.a[current]==0) {
               if(var[m][i][j][k] > ranges[2*m] && var[m][i][j][k] < ranges[2*m+1]) { 
-                c.a[current]=colors[m];
+                a.a[current]=colors[m];
               }
             }
           }
@@ -71,13 +69,13 @@ int main() {
   }         
 
   for(int i=0;i<10;i++) {
-    cout << x.a[i] << " " << y.a[i] << " " << z.a[i] << " " << c.a[i] << endl;
+    cout << a.a[i] << endl;
   }
 
 
-  for(int i=0;i<3;i++) {
-      for(int j=0;j<100;j++) {
-	  for(int k=0;k<100;k++) {
+  for(int i=0;i<num_vars;i++) {
+      for(int j=0;j<dim_size;j++) {
+	  for(int k=0;k<dim_size;k++) {
 		delete[] var[i][j][k];
 	  }
 	delete[] var[i][j];
